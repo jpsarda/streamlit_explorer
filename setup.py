f4f7ba4b21bc4e6373abbd737f5f30eb513e2d79
Oleg <oleg.rez@gmail.com>
2026-01-03 16:02:39 +0200

    First commit

---
+++ setup.py
@@ -0,0 +1,37 @@
+from setuptools import setup, find_packages
+
+with open("README.md", "r", encoding="utf-8") as fh:
+    long_description = fh.read()
+
+setup(
+    name="streamlit_explorer",
+    version="0.1.0",
+    author="Rezinski Oleg",
+    author_email="",
+    description="A custom file and folder browser for Streamlit applications",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="",
+    packages=find_packages(),
+    classifiers=[
+        "Development Status :: 3 - Alpha",
+        "Intended Audience :: Developers",
+        "Topic :: Software Development :: Libraries :: Python Modules",
+        "License :: OSI Approved :: MIT License",
+        "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: 3.8",
+        "Programming Language :: Python :: 3.9",
+        "Programming Language :: Python :: 3.10",
+        "Programming Language :: Python :: 3.11",
+        "Framework :: Streamlit",
+    ],
+    python_requires=">=3.8",
+    install_requires=[
+        "streamlit>=1.31.0",
+    ],
+    keywords="streamlit file-picker folder-picker directory-picker file-browser file-explorer folder-explorer",
+    project_urls={
+        # "Bug Reports": "https://github.com/yourusername/streamlit-path-picker/issues",
+        # "Source": "https://github.com/yourusername/streamlit-path-picker",
+    },
+)
